d06276eb142db22793bf716a9d0ffa58ffac4786
SEG <segdev.mx@gmail.com>
2022-12-15 05:19:31 -0600

    feat: add challenge of day 12 of year 2022

---
+++ 2022/day12/README.md
@@ -0,0 +1,2 @@
+# 2022 | Day 12
+https://adventofcode.com/2022/day/12
---
+++ 2022/day12/index.js
@@ -0,0 +1,108 @@
+const fs = require("fs");
+const input = fs.readFileSync("./input.txt", "utf-8").trim();
+
+let grid = [];
+
+for (let line of input.split("\n")) {
+  grid.push(line.split(""));
+}
+
+function getIndexesOfLetter(letter) {
+  for (let x = 0; x < grid[0].length; x++) {
+    for (let y = 0; y < grid.length; y++) {
+      if (grid[y][x] == letter) {
+        return {x: x, y: y};
+      }
+    }
+  }
+}
+
+const startIndexes = getIndexesOfLetter("S");
+const endIndexes = getIndexesOfLetter("E");
+
+grid[startIndexes.y][startIndexes.x] = "a";
+grid[endIndexes.y][endIndexes.x] = "z";
+
+const dijkstraMap = new Map();
+
+function isAccessible(from, to) {
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  if (alphabet.indexOf(to) - 1 <= alphabet.indexOf(from)) {
+    return true;
+  }
+
+  return false;
+}
+
+function visitSlot(x, y, currentSteps) {
+  const mapKey = x + "-" + y;
+  if (
+    dijkstraMap.has(mapKey) &&
+    dijkstraMap.get(mapKey) <= currentSteps
+  ) {
+    return;
+  } else {
+    dijkstraMap.set(mapKey, currentSteps);
+  }
+
+  const nextPositions = [
+    {x: x, y: y-1}, // up
+    {x: x, y: y+1}, // down
+    {x: x+1, y: y}, // right
+    {x: x-1, y: y}, // left
+  ];
+
+  for (let pos of nextPositions) {
+    if (!grid[pos.y] || !grid[pos.y][pos.x]) {
+      continue;
+    }
+
+    let slot = grid[pos.y][pos.x];
+    if (slot && isAccessible(grid[y][x], slot)) {
+      visitSlot(pos.x, pos.y, currentSteps + 1);
+    }
+  }
+}
+
+visitSlot(startIndexes.x, startIndexes.y, 0);
+let minimumSteps = dijkstraMap.get(endIndexes.x + "-" + endIndexes.y);
+
+console.log(
+  "The minimum steps of getting from start to end is:",
+  minimumSteps
+);
+
+let stepsArray = [minimumSteps];
+let startPositions = [];
+
+for (let x = 0; x < grid[0].length; x++) {
+  for (let y = 0; y < grid.length; y++) {
+    if (grid[y][x] == "a") {
+      startPositions.push({x: x, y: y});
+    }
+  }
+}
+
+startPositions = startPositions.filter(p => {
+  return !(p.x == startIndexes.x && p.y == startIndexes.y)
+});
+
+for (let pos of startPositions) {
+  dijkstraMap.clear();
+  visitSlot(pos.x, pos.y, 0);
+  stepsArray.push(dijkstraMap.get(endIndexes.x + "-" + endIndexes.y))
+}
+
+minimumSteps = null;
+
+for (let steps of stepsArray) {
+  if (minimumSteps == null || minimumSteps > steps) {
+    minimumSteps = steps;
+  }
+}
+
+console.log(
+  "The minimum steps starting from anywhere is:",
+  minimumSteps
+);
