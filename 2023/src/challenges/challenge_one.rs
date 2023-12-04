cd14342abbb8eabd40f97421c8dfbe1f40605ff5
SEG <segdev.mx@gmail.com>
2023-12-03 21:26:21 -0600

    feat: add first challenge of 2023

--- 2023/src/challenges/challenge_one.rs
+++ 2023/src/challenges/challenge_one.rs
@@ -8,6 +8,82 @@ impl Challenge for ChallengeOne {
     }
 
     fn run(&self, input: String) -> Vec<String> {
-        todo!()
+        let first_part_result = calculate_vibration_value(&input).to_string();
+
+        let input = input.lines().map(to_numbers).collect::<Vec<_>>();
+        let second_part_result = calculate_vibration_value(&input.join("\n"));
+
+        vec![
+            format!("First part: {first_part_result}"),
+            format!("Second part: {second_part_result}"),
+        ]
+    }
+}
+
+fn calculate_vibration_value(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let mut iter = line.chars().filter(|value| value.is_ascii_digit());
+
+            let zero_ascii = '0' as u32;
+            let first = iter.next().unwrap();
+            let last = iter.last().unwrap_or(first);
+
+            let first = first as u32 - zero_ascii;
+            let last = last as u32 - zero_ascii;
+
+            first * 10 + last
+        })
+        .sum::<u32>()
+}
+
+fn to_numbers(line: &str) -> String {
+    let mut buffer = String::new();
+
+    for start in 0..line.len() {
+        let value = line.chars().nth(start).unwrap();
+
+        if value.is_ascii_digit() {
+            buffer.push(value);
+            continue;
+        }
+
+        for end in start..=line.len() {
+            let substring = &line[start..end];
+
+            let num = match substring {
+                "one" => '1',
+                "two" => '2',
+                "three" => '3',
+                "four" => '4',
+                "five" => '5',
+                "six" => '6',
+                "seven" => '7',
+                "eight" => '8',
+                "nine" => '9',
+                _ => continue,
+            };
+
+            buffer.push(num);
+        }
+    }
+
+    buffer
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn challenge_one() {
+        let challenge = ChallengeOne;
+        let input = std::fs::read_to_string(challenge.path(crate::Environment::Local)).unwrap();
+
+        assert_eq!(
+            challenge.run(input),
+            vec!["First part: 234", "Second part: 281"]
+        );
     }
 }
