17867957bb6f1e765171df738bec1066f1af1833
SEG <segdev.mx@gmail.com>
2023-12-03 19:04:44 -0600

    feat: add the section of code that runs the challenges and prints their outputs

---
+++ 2023/src/challenges/challenge_one.rs
@@ -0,0 +1,13 @@
+use crate::{challenge::Challenge, Day};
+
+pub struct ChallengeOne;
+
+impl Challenge for ChallengeOne {
+    fn day(&self) -> Day {
+        Day::from(1).unwrap()
+    }
+
+    fn run(&self, input: String) -> Vec<String> {
+        todo!()
+    }
+}
--- 2023/src/lib.rs
+++ 2023/src/lib.rs
@@ -1,10 +1,29 @@
+use std::fs;
+
+use challenge::Challenge;
+use challenges::prelude::*;
 pub use day::{Day, DayError};
 pub use environment::Environment;
 
 pub mod challenge;
+mod challenges;
 mod day;
 mod environment;
 
 pub fn run(day: Day, environment: Environment) {
-    todo!()
+    let day = *day;
+
+    let day: Box<dyn Challenge> = Box::new(match day {
+        1 => ChallengeOne,
+        _ => unimplemented!("Missing solution for challenge {day}"),
+    });
+
+    let path = day.path(environment);
+
+    let input = fs::read_to_string(&path)
+        .unwrap_or_else(|_| panic!("Failed to read input file {:?}", path.file_name()));
+
+    day.run(input.trim().to_string())
+        .iter()
+        .for_each(|i| println!("{i}"));
 }
